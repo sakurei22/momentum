da86214b713d1167d1f3498b23043a4dcd44d279
sj <sakurei22@gmail.com>
2022-03-31 17:53:42 +0900

    예쁘게 만들기

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock(){
     const date = new Date();
-    clock.innerHTML =`${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const min = String(date.getMinutes()).padStart(2, "0");
+    const sec = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML =`${hours} : ${min} : ${sec}`;
 }
 
 getClock();
