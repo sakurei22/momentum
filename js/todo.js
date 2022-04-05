752b29835ccbd490013e1e7430a924e88e7084a5
sj <sakurei22@gmail.com>
2022-04-05 09:47:41 +0900

    adding todo

--- js/todo.js
+++ js/todo.js
@@ -6,6 +6,16 @@ function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newTodo);
+}
+
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+
+    li.appendChild(span);
+    span.innerHTML = newTodo;
+    toDoList.appendChild(li);
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
